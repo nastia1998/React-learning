f9cb35076bcde14a766072852275c80588ff916f
Anastasiya Leshchanka <a.leshchanka@LeverX.local>
2019-09-04 19:11:26 +0300

    create info component

---
+++ cities/src/components/info.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Weather application</h2>
+                <p>Find out the weather in you city!</p>
+            </div>
+        );
+    }
+}
