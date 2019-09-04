2e821abe37a9db98c64bcbe843d2863bac884310
Anastasiya Leshchanka <a.leshchanka@LeverX.local>
2019-09-04 19:23:23 +0300

    add Form and Weather components

--- cities/src/App.js
+++ cities/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Info from "./components/Info";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Info />
+                <Form />
+                <Weather />
             </div>
         );
     }
---
+++ cities/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div>
+                Component with Form
+            </div>
+        );
+    }
+}
+
+export default Form;
