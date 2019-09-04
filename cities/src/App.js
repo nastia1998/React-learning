582d9596fc035b728f7d4d62136aa9eef1db9eac
Anastasiya Leshchanka <a.leshchanka@LeverX.local>
2019-09-04 19:17:17 +0300

    add component Info to App

---
+++ cities/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Info from "./components/Info";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Info />
+            </div>
+        );
+    }
+}
+
+export default App;
